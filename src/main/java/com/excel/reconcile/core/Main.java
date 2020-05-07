5d43d5a9bc12b3c81b8a20751506ef28d7e4d994
Pradeep Panayal <pradeeppanayal@gmail.com>
2020-05-07 23:09:14 +0530

    logger changed

--- src/main/java/com/excel/reconcile/core/Main.java
+++ src/main/java/com/excel/reconcile/core/Main.java
@@ -3,7 +3,8 @@ package com.excel.reconcile.core;
 import com.excel.beans.Document;
 import com.excel.beans.ReconsileResult;
 import org.apache.commons.cli.*;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.security.InvalidParameterException;
@@ -14,7 +15,7 @@ import java.security.InvalidParameterException;
  */
 
 public class Main {
-    private static final Logger LOGGER = Logger.getLogger(Main.class);
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException, ParseException {
 
